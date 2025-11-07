bfbe01572e055a74d9612e393295dba351f034b4
Anil Ruhela <anilruhela@Anils-MacBook-Air.local>
2025-11-07 15:46:34 +0530

    Fix: changed id type to String in Enquiry model

--- src/models/Enquiry.js
+++ src/models/Enquiry.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const enquirySchema = new mongoose.Schema(
   {
-    id:{type:Number,required:true,unique:true},
+    id: { type: String, required: true, unique: true },
+
     name: { type: String, required: true },
     email: { type: String, required: true },
     mobile: { type: String, required: true },
